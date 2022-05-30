f25299771de6f9c14651f5d93cdbbf87864d7c73
adikonen <addykonen80@gmail.com>
2022-05-30 23:11:09 +0800

    menbuat halaman login

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Laravel\Socialite\SocialiteServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -191,7 +191,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
+        // 'ExampleClass' => App\Example\ExampleClass::class,\
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,  
     ])->toArray(),
 
 ];
